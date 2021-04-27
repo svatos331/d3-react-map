8762fa2ae2a50c2c1a7c730f342eb9eb2ee51117
Петр Молчанов <p.molchanov@profsoft.pro>
2021-04-27 18:49:46 +0400

    add cities

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,185 +1,275 @@
-import React, {useRef, useEffect, useState} from "react";
-import * as d3 from 'd3'
+import React, { useRef, useEffect, useState } from "react";
+import * as d3 from "d3";
 
 import "./style.css";
-import geojson from './geojson.json';
-import {select} from "d3-selection";
+import geojson from "./geojson.json";
+import regions from "./ua-cities.json";
+import { select } from "d3-selection";
 
+const category_0 = "#ff000000";
+const category_1 = "#b32f4a";
+const category_2 = `#d7313e`;
+const category_3 = `#f97041`;
+const category_4 = `#faa247`;
+const category_5 = `#acd6a9`;
 
-const category_0 = '#ff000000'
-const category_1 = '#b32f4a'
-const category_2 = `#d7313e`
-const category_3 = `#f97041`
-const category_4 = `#faa247`
-const category_5 = `#acd6a9`
+function paramsColors(num) {
+  if (num >= 0 && num <= 50) {
+    return category_5;
+  }
 
-function paramsColors(num){
-    if((num >= 0)&& (num <= 50)){
-        return category_5
-    }
+  if (num >= 51 && num <= 70) {
+    return category_4;
+  }
 
-    if((num >= 51)&& (num <= 70)){
-        return category_4
-    }
+  if (num >= 71 && num <= 90) {
+    return category_3;
+  }
 
-    if((num >= 71)&& (num <= 90)){
-        return category_3
-    }
+  if (num >= 91 && num <= 120) {
+    return category_2;
+  }
 
-    if((num >= 91)&& (num <= 120)){
-        return category_2
-    }
-
-    if(num >= 121){
-        return category_1
-    }
-
+  if (num >= 121) {
+    return category_1;
+  }
 }
-
-
-const randomColor = () => {
-    return "#" +(Math.random().toString(16) + '000000').substring(2,8).toUpperCase()
-}
-
-
-
-
-
 
 const Map = (props) => {
+  const zoomHandler = (newZoom) => {
+    // zoom.zoom > newZoom? setZoom(newZoom,(zoom.scale - 0.4)) : setZoom(newZoom,(zoom.scale + 0.4))
 
-    const [zoom, setZoom] = useState({
-        zoom: '1'
-    })
+    return 4 - newZoom / 2 < 0.5 ? 0.5 : 4 - newZoom / 2;
+  };
 
-    const zoomHandler = (newZoom) => {
-        // zoom.zoom > newZoom? setZoom(newZoom,(zoom.scale - 0.4)) : setZoom(newZoom,(zoom.scale + 0.4))
+  // const [{ data }] = useDataApi(url,[]);
+  const data = geojson;
+  const svgRef = useRef();
+  const projRef = useRef(d3.geoMercator().center([32.07, 49.23]).scale(4700));
 
-        return (4 - newZoom/2) < 0.5? 0.5: (4 - newZoom/2)
+  // console.log(projRef)
+
+  // CREATES THE PROJECTION AND RENDERS CHART AND PARKS
+  useEffect(() => {
+    // GRAB CURRENT WIDTH/HEIGHT OF DIV ID="MAP"
+    const height = svgRef.current.clientHeight;
+    const width = svgRef.current.clientWidth;
+    // FINE TUNE THE POSITION THE MAP WITHING THE ELEMENT
+    projRef.current.translate([width / 2, height / 2]);
+
+    // ASSING THE PROJECTION A PROJECTION
+    const path = d3.geoPath().projection(projRef.current);
+    if (data.length) {
+      renderChart(data[0].features, path);
     }
+  }, [data]);
 
-
-
-
-    // const [{ data }] = useDataApi(url,[]);
-    const data = geojson
-    const svgRef = useRef();
-    const projRef = useRef(d3.geoMercator().center([32.07, 49.23]).scale(4700));
-
-    // console.log(projRef)
-
-    // CREATES THE PROJECTION AND RENDERS CHART AND PARKS
-    useEffect(() => {
-        // GRAB CURRENT WIDTH/HEIGHT OF DIV ID="MAP"
-        const height = svgRef.current.clientHeight;
-        const width = svgRef.current.clientWidth;
-        // FINE TUNE THE POSITION THE MAP WITHING THE ELEMENT
-        projRef.current.translate([width  / 2, height  / 2 ]);
-
-        // ASSING THE PROJECTION A PROJECTION
-        const path = d3.geoPath().projection(projRef.current);
-        if (data.length) {
-            renderChart(data[0].features, path);
-        }
-    }, [data]);
-
-// UPDATES THE MAP ONLY IF props.activeParks HAS CHANGED
-    useEffect(() => {
-
-        if (props.data.data){
-            renderParks(props.data.data)
-        }
-
-    },[props.data])
-
-// RENDER THE MAP
-
-
-
-
-
-    const renderChart = (data, path) => {
-
-     let g =  d3.select(svgRef.current).selectAll('path').data(data).enter()
-            .append('path')
-            .attr('class', (d) => {
-                return d.properties.name
-            })
-            .attr('d', path)
-            .attr('stroke', 'black')
-        
-
-            .style('fill', (d) => '#f5f5dc')
-            .attr('title', (d) => d.properties.name)
-            .on('mouseout', (e) => {
-                props.titleHandler({show: false, text: ''})
-                select(e.target)
-                    .attr('opacity', '1')
-            })
-            .on('mouseenter', (e) => {
-                props.titleHandler({show: true, text: e.target.getAttribute('title')})
-                select(e.target)
-                    .attr('opacity', '0.7')
-            })
-            // .call(d3.zoom()
-            //     .extent([[0, 0], [1000, 1000]])
-            //     .scaleExtent([1, 8])
-            //     .on("zoom", zoomed));
-
-
-    };
-
-    // .split(',')[0]
-    
-
-// RENDER THE PARKS
-    const renderParks = (parks) => {
-        const circles = d3.select(svgRef.current).selectAll('.parks').data(parks, (d) => d.name);
-        circles.enter().append('g').append('circle')
-            .attr('transform', d => 'translate(' + projRef.current([+d.longitude, +d.latitude]) + ')') 
-            .attr('r', 4)
-            .on('mouseout', (e) => {
-                props.titleHandler({show: false, text: ''})
-            })
-            .on('mouseenter', (e) => {
-               
-                props.titleHandler({show: true, air: e.target.getAttribute('data-air'),  text: e.target.getAttribute('title')})
-            })
-            .attr('class', (d, i) => `parks park-${d.id}`)
-            .attr('title', (d) => {
-                return d.cityName
-            })
-            .attr('data-air', (d) => {
-                return d.pollutants[d.pollutants.length - 1] ? d.pollutants[d.pollutants.length - 1].value : null
-              
-            })
-            .style('fill', (d) => paramsColors(d.pollutants[d.pollutants.length - 1].value))
-            .style('opacity', 0)
-            .transition().duration(500).style('opacity',1)
-    };
-
-    
-
-
-    function zoomed({ transform }) {
-        d3.select(svgRef.current).selectAll('path').attr("transform", transform);
-        d3.select(svgRef.current).selectAll('g').attr("transform", transform);
-        d3.select(svgRef.current).selectAll('circle').attr('r', zoomHandler(transform.k));
-
- 
-        
-
-
+  // UPDATES THE MAP ONLY IF props.activeParks HAS CHANGED
+  useEffect(() => {
+    if (props.data.data) {
+      renderParks(props.data.data);
     }
-    const xg = d3.select(svgRef.current).call(d3.zoom()
-        .extent([[0, 0], [10000000, 1000000]])
-        // .scaleExtent([1, 8])
-        .on("zoom", zoomed));
+  }, [props.data]);
 
+  // RENDER THE MAP
+  useEffect(() => {
+    // console.log(JSON.parse(regions));
+    // console.log(regions);
+    regions[0].regions.forEach((region) => {
+      // console.log(region.cities);
+      renderCties(region.cities);
+    });
+  }, []);
 
-    return (
-        <svg style={{"transition":" 0.8s"}} width={10} height={10}  id="boroughs-map" ref={svgRef}></svg>
-    );
+  const renderChart = (data, path) => {
+    let g = d3
+      .select(svgRef.current)
+      .selectAll("path")
+      .data(data)
+      .enter()
+      .append("path")
+      .attr("class", (d) => {
+        return d.properties.name;
+      })
+      .attr("d", path)
+      .attr("stroke", "black")
+
+      .style("fill", (d) => "#f5f5dc")
+      .attr("title", (d) => d.properties.name)
+      .on("mouseout", (e) => {
+        props.titleHandler({ show: false, text: "" });
+        select(e.target).attr("opacity", "1");
+      })
+      .on("mouseenter", (e) => {
+        props.titleHandler({
+          show: true,
+          text: e.target.getAttribute("title"),
+        });
+        select(e.target).attr("opacity", "0.7");
+      });
+  };
+
+  // .split(',')[0]
+
+  // RENDER THE PARKS
+  const renderParks = (parks) => {
+    const circles = d3
+      .select(svgRef.current)
+      .selectAll(".parks")
+      .data(parks, (d) => d.name);
+    circles
+      .enter()
+      .append("g")
+      .append("circle")
+      .attr(
+        "transform",
+        (d) => "translate(" + projRef.current([+d.longitude, +d.latitude]) + ")"
+      )
+      .attr("r", 4)
+      .on("mouseout", (e) => {
+        props.titleHandler({ show: false, text: "" });
+      })
+      .on("mouseenter", (e) => {
+        props.titleHandler({
+          show: true,
+          air: e.target.getAttribute("data-air"),
+          text: e.target.getAttribute("title"),
+        });
+      })
+      .attr("class", (d, i) => `parks park-${d.id}`)
+      .attr("title", (d) => {
+        return d.cityName;
+      })
+      .attr("data-air", (d) => {
+        return d.pollutants[d.pollutants.length - 1]
+          ? d.pollutants[d.pollutants.length - 1].value
+          : null;
+      })
+      .style("fill", (d) =>
+        paramsColors(d.pollutants[d.pollutants.length - 1].value)
+      )
+      .style("opacity", 0)
+      .transition()
+      .duration(500)
+      .style("opacity", 1);
+  };
+
+  const renderCties = (cities) => {
+    const circles = d3
+      .select(svgRef.current)
+      .selectAll(".cities")
+      .data(cities, (d) => d.name);
+    circles
+      .enter()
+      .append("g")
+      .append("circle")
+      .attr(
+        "transform",
+        (d) => "translate(" + projRef.current([+d.lng, +d.lat]) + ")"
+      )
+      // .attr("class", () => "class")
+      .attr("r", 4)
+      .on("mouseout", (e) => {
+        props.titleHandler({ show: false, text: "" });
+      })
+      .on("mouseenter", (e) => {
+        props.titleHandler({
+          show: true,
+          air: e.target.getAttribute("data-air"),
+          text: e.target.getAttribute("title"),
+        });
+      })
+      .attr("class", (d, i) => `parks park-${d.name} city`)
+      // .attr("style", (d, i) => `parks park-${d.name} city`)
+      .attr("title", (d) => {
+        return d.name;
+      })
+      // .text(() => "город")
+      // .attr("data-air", (d) => {
+      //     return d.pollutants[d.pollutants.length - 1]
+      //         ? d.pollutants[d.pollutants.length - 1].value
+      //         : null;
+      // })
+      .style("fill", (d) => "#00000")
+      .style("opacity", 0)
+      .transition()
+      .duration(500)
+      .style("opacity", 1);
+
+    const texts = d3
+      .select(svgRef.current)
+      .selectAll(".texts")
+      .data(cities, (d) => d.name);
+    texts
+      .enter()
+      .append("g")
+      .append("text")
+      .attr(
+        "transform",
+        (d) => "translate(" + projRef.current([+d.lng, +d.lat]) + ")"
+      )
+      .text((d) => d.name)
+      .transition()
+      // .attr("class", () => "class")
+      // .attr("r", 4)
+      // .on("mouseout", (e) => {
+      //   props.titleHandler({ show: false, text: "" });
+      // })
+      // .on("mouseenter", (e) => {
+      //   props.titleHandler({
+      //     show: true,
+      //     air: e.target.getAttribute("data-air"),
+      //     text: e.target.getAttribute("title"),
+      //   });
+      // })
+      // .attr("class", (d, i) => `parks park-${d.name} city`)
+      // // .attr("style", (d, i) => `parks park-${d.name} city`)
+      .attr("title", (d) => {
+        return d.name;
+      })
+      // .text(() => "город")
+      // .attr("data-air", (d) => {
+      //     return d.pollutants[d.pollutants.length - 1]
+      //         ? d.pollutants[d.pollutants.length - 1].value
+      //         : null;
+      // })
+      .style("fill", (d) => "#00000")
+      .style("opacity", 0)
+      .transition()
+      .duration(500)
+      .style("opacity", 1);
+  };
+
+  function zoomed({ transform }) {
+    d3.select(svgRef.current).selectAll("path").attr("transform", transform);
+    d3.select(svgRef.current).selectAll("g").attr("transform", transform);
+    d3.select(svgRef.current)
+      .selectAll("circle")
+      .attr("r", zoomHandler(transform.k));
+    d3.select(svgRef.current)
+      .selectAll("text")
+      .attr("font-size", zoomHandler(transform.k) * 6);
+  }
+  const xg = d3.select(svgRef.current).call(
+    d3
+      .zoom()
+      .extent([
+        [0, 0],
+        [10000000, 1000000],
+      ])
+      // .scaleExtent([1, 8])
+      .on("zoom", zoomed)
+  );
+
+  return (
+    <svg
+      style={{ transition: " 0.8s" }}
+      width={10}
+      height={10}
+      id="boroughs-map"
+      ref={svgRef}
+    ></svg>
+  );
 };
 
 export default Map;
--- src/index.js
+++ src/index.js
@@ -1,75 +1,58 @@
-
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { render } from "react-dom";
 // import Map_old from "./Map_old";
 import Map from "./Map";
 import "./style.css";
 import Loader from "./Loader";
 
-
-
-
 function App() {
-const [pointsLists, pointsTodos] = React.useState([]);
-const [loading, setLoading] = React.useState(true);
-const [title, setTitle] = useState({
+  const [pointsLists, pointsTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [title, setTitle] = useState({
     show: false,
-    text: '',
-    air: ''
-})
-    const titleHandler = (title, indexAir) => {
-    setTitle(title,indexAir)
-    }
+    text: "",
+    air: "",
+  });
+  const titleHandler = (title) => {
+    setTitle(title);
+  };
 
-    const mapRef = useRef();
+  const mapRef = useRef();
 
-useEffect(()=> {
+  useEffect(() => {
     if (loading) {
-        fetch('http://localhost:3001/getJson')
-            .then(response => response.json())
-            .then(json => {
-                setTimeout(()=> {
-                    setLoading(false)
-                    pointsTodos(json)
-                }, 2000)
-            })
+      fetch("http://localhost:3001/getJson")
+        .then((response) => response.json())
+        .then((json) => {
+          setTimeout(() => {
+            setLoading(false);
+            pointsTodos(json);
+          }, 2000);
+        });
     }
-
-    
-
-}, []) 
-
+  }, []);
 
   return (
     <>
-        {title.show ? <div className="title">{title.text} : {title.air}</div>: null }
+      {title.show ? <div className="title">{title.text}</div> : null}
 
-       <div className="window">
-        <div className="category_1">
-        AQI: от 100 до макс
-        </div>
-        <div className="category_2">
-        AQI: от 90 до 120
-        </div>
-        <div className="category_3">
-        AQI: от 70 до 90
-        </div>
-        <div className="category_4">
-        AQI: от 50 до 70
-        </div>
-        <div className="category_5">
-        AQI: от 0 до 50
-        </div>
-        </div>
+      <div className="window">
+        <div className="category_1">AQI: от 100 до макс</div>
+        <div className="category_2">AQI: от 90 до 120</div>
+        <div className="category_3">AQI: от 70 до 90</div>
+        <div className="category_4">AQI: от 50 до 70</div>
+        <div className="category_5">AQI: от 0 до 50</div>
+      </div>
 
-        {loading && <Loader />}
-        {/*{pointsLists.length ? null : ( loading? null : <Map_old points={pointsLists} />) }  */}
-        {/*{pointsLists.length ? <Map_old data={pointsLists}/> : null}*/}
-        {!loading ? <div id="map" ref={mapRef}>
-            <Map data ={pointsLists} titleHandler={titleHandler}/>
-        </div> : null}
-
-       </>
-   );
+      {loading && <Loader />}
+      {/*{pointsLists.length ? null : ( loading? null : <Map_old points={pointsLists} />) }  */}
+      {/*{pointsLists.length ? <Map_old data={pointsLists}/> : null}*/}
+      {!loading ? (
+        <div id="map" ref={mapRef}>
+          <Map data={pointsLists} titleHandler={titleHandler} />
+        </div>
+      ) : null}
+    </>
+  );
 }
 render(<App />, document.getElementById("root"));
